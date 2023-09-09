7227a23e37e0986126648283b92ae8bbd019086a
안강민 <hkkm612@gmail.com>
2023-09-09 17:05:07 +0900

    create. [APP/config] 실행 환경 별 .env 파일 읽어 환경 변수 처리. 현재는 PORT만 설정

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { envKey } from './config/constant';
 
 async function bootstrap() {
-  const port = 3000;
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>(envKey.port);
+
   await app.listen(port);
   console.log(`🟩 Listening on http://localhost:${port}`);
 }
