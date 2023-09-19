4e0ea74c7fb588cfa8650f91acf4cbaa71bf0b37
안강민 <hkkm612@gmail.com>
2023-09-19 15:09:04 +0900

    update. [User/Entity] User Entity의 속성 주석 수정

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -5,7 +5,7 @@ export class User {
   @PrimaryGeneratedColumn('uuid')
   id: number;
 
-  // 활동 사용자 여부
+  // 활동 여부
   @Column({ default: true })
   isActive: boolean;
 
@@ -22,13 +22,13 @@ export class User {
   name: string;
 
   // 프로필 이미지 사진 저장 경로
-  @Column()
+  @Column({ nullable: true })
   profileImageUrl: string;
 
   @Column({ type: 'date', nullable: true })
   birthDate: Date;
 
-  // 사용자의 성인 여부
+  // 성인 여부
   @Column({ default: false })
   isAdult: boolean;
 }
