689cb8d90db422e72724dc5cde95313d5fb48b6f
milkymilky0116 <sjlee990129@gmail.com>
2023-12-28 16:17:26 +0900

    chore: #18 fix minor issue on swagger

--- src/main.ts
+++ src/main.ts
@@ -6,23 +6,24 @@ import { AllExceptionFilter } from './common/filter/exception.filter';
 async function bootstrap() {
   const port = 3000;
   const app = await NestFactory.create(AppModule);
-
+  app.setGlobalPrefix('api/v1');
   const config = new DocumentBuilder()
+    .addServer('api/v1')
     .setTitle('Goraeph Backend API')
     .setDescription('Blog Service Goraeph API Description')
     .setVersion('0.1')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
 
+  SwaggerModule.setup('api/v1/docs', app, document);
   app.useGlobalPipes(
     new ValidationPipe({
       stopAtFirstError: true,
     }),
   );
   app.useGlobalFilters(new AllExceptionFilter());
-  app.setGlobalPrefix('api/v1');
+
   await app.listen(port);
   console.log(`🟩 Listening on http://localhost:${port}`);
 }
